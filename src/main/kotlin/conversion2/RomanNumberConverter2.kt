4e372ea1c4e713a9e573afda6c4fbc325f4ef687
Mariusz Zawadzki <mariusz.r.zawadzki@gmail.com>
2019-04-19 00:14:04 +0200

    second approach. added some validation. Still not complete.

--- src/main/kotlin/conversion2/RomanNumberConverter2.kt
+++ src/main/kotlin/conversion2/RomanNumberConverter2.kt
@@ -1,21 +1,52 @@
 package conversion2
 
-import java.lang.IllegalArgumentException
+import conversion2.RomanNumberConverter2.mapToInt
+import conversion2.RomanNumberConverter2.replaceDescending
 
 object RomanNumberConverter2 {
     fun convert(roman: String): Int {
-        return roman.map {
-            when(it){
-                'I' -> 1
-                'V' -> 5
-                'X' -> 10
-                'L' -> 50
-                'C' -> 100
-                'D' -> 500
-                'M' -> 1000
-                else -> throw IllegalArgumentException("illegal character '$it' in roman number ")
-            }
+        roman.validRomanNumber()
+        return replaceDescending(roman)
+            .map {
+                mapToInt(it)
         }.sum()
+    }
+
+    fun mapToInt(it: Char): Int {
+        return when (it) {
+            'I' -> 1
+            'V' -> 5
+            'X' -> 10
+            'L' -> 50
+            'C' -> 100
+            'D' -> 500
+            'M' -> 1000
+            else -> throw IllegalArgumentException("illegal character '$it' in roman number ")
+        }
+    }
+
+    fun replaceDescending(roman: String): String {
+        return roman
+            .replace("IV", "IIII")
+            .replace("IX", "VIIII")
+            .replace("XL", "XXXX")
+            .replace("XC", "LXXXX")
+            .replace("CD", "CCCC")
+            .replace("CM", "DCCCC")
+    }
+
+}
+
+val validDescending = listOf("I","V","X","L","C","D","M")
+
+private fun String.validRomanNumber() {
+    if (this.matches(Regex(".*[IVXLCDM]{3}[IVXLCDM]+"))){
+        throw IllegalArgumentException("$this is not a valid roman number")
+    }
+    val replaceDescending = replaceDescending(this)
+    val sortedBy = replaceDescending.toList().sortedBy { -mapToInt(it) }.joinToString(separator="") {it.toString()}
+    if(sortedBy != replaceDescending) {
+        throw IllegalArgumentException("$this is not a valid roman number")
     }
 
 }
--- src/test/kotlin/RomanNumberConverter2Spec.kt
+++ src/test/kotlin/RomanNumberConverter2Spec.kt
@@ -2,6 +2,7 @@ import conversion2.RomanNumberConverter2
 import org.assertj.core.api.Assertions
 import org.spekframework.spek2.Spek
 import org.spekframework.spek2.style.specification.describe
+import java.lang.IllegalArgumentException
 import kotlin.math.exp
 
 object RomanNumberConverter2Spec : Spek({
@@ -17,11 +18,52 @@ object RomanNumberConverter2Spec : Spek({
             Pair("C",100),
             Pair("D",500),
             Pair("M",1000)
-            )
+        )
 
         numbers.forEach { (roman, expected) ->
             it("convets $roman to $expected") {
                 Assertions.assertThat(RomanNumberConverter2.convert(roman)).isEqualTo(expected)
+            }
+        }
+    }
+
+    describe("converts properly ascending letters") {
+        val numbers = listOf(
+            Pair("IV",4),
+            Pair("IX",9),
+            Pair("XL",40),
+            Pair("XC",90),
+            Pair("CD",400),
+            Pair("CM",900)
+        )
+
+        numbers.forEach { (roman, expected) ->
+            it("convets $roman to $expected") {
+                Assertions.assertThat(RomanNumberConverter2.convert(roman)).isEqualTo(expected)
+            }
+        }
+    }
+
+    describe("does not convert an invalid string") {
+        val numbers = listOf(
+            "IIII",
+            "VVVV",
+            "XXXX",
+            "LLLL",
+            "CCCC",
+            "DDDD",
+            "MMMM",
+            "VL",
+            "IL",
+            "IM"
+        )
+
+        numbers.forEach { roman ->
+            it("does not convert $roman ") {
+                Assertions.assertThatThrownBy {
+                    RomanNumberConverter2.convert(roman)
+                }.isInstanceOf(IllegalArgumentException::class.java)
+                    .hasMessage("$roman is not a valid roman number")
             }
         }
     }
